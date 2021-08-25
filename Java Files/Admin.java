74000d3f235255bb7eb990c6982523b54987e7e4
AppSync-ai <81430894+AppSync-ai@users.noreply.github.com>
2021-08-25 11:55:37 +0530

    Create Admin.java

---
+++ Java Files/Admin.java
@@ -0,0 +1,53 @@
+package com.mt.spingamer.AllModules;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.mt.spingamer.R;
+
+public class Admin {
+    public static String BASE_URL = "";
+    public static TinyDB tinyDB;
+    public static String appname = "";
+
+    public static void intializeLocalRoom(Context context) {
+        tinyDB = new TinyDB(context);
+    }
+
+    public static void OverrideNow(Context context) {
+        ((Activity) context).overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    }
+
+    public static void HandleToolBar(Context context, String title, ImageView backImg, TextView titleTxt) {
+
+        titleTxt.setText("" + title);
+        backImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Activity) context).finish();
+                OverrideNow(context);
+            }
+        });
+
+    }
+
+
+    public static void loadFrag(Fragment f1, String name, FragmentManager fm) {
+        for (int i = 0; i < fm.getBackStackEntryCount(); ++i) {
+            fm.popBackStack();
+        }
+        FragmentTransaction ft = fm.beginTransaction();
+        //  ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft.replace(R.id.container, f1, name);
+        ft.commit();
+
+    }
+
+}
