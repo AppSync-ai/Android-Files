0c2417c06beb94bcefd81bbea3554172a1670544
AppSync-ai <81430894+AppSync-ai@users.noreply.github.com>
2021-10-05 18:24:51 +0530

    ds

--- Java Files/For Retrofit/Gson Model/ModelBlankGson.java
+++ Java Files/For Retrofit/Gson Model/ModelBlankGson.java
@@ -1,20 +1,13 @@
 package com.mt.spingamer.Models;
 
-import static android.util.Log.*;
 import static android.util.Log.wtf;
 import static com.mt.spingamer.AllModules.Admin.service;
-
-import android.util.Log;
 
 import androidx.lifecycle.MutableLiveData;
 
 import com.google.gson.Gson;
-import com.mt.spingamer.AllModules.Admin;
+import com.mt.spingamer.AllReqs.BannersReq;
 import com.mt.spingamer.AllReqs.SocialReq;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -23,10 +16,9 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 
-public class ModelSocial {
+public class ModelBanners {
     public static MutableLiveData<String> message = new MutableLiveData<>();
-    public static MutableLiveData<String> response_data = new MutableLiveData<>();
-    public static ArrayList<String> list = new ArrayList<>();
+    public static MutableLiveData<BannersReq> response_data = new MutableLiveData<>();
 
     public static void load() {
         Call<String> call = service.get_socials_v2();
@@ -36,21 +28,25 @@ public class ModelSocial {
                 if (response.isSuccessful()) {
                     try {
                         Gson gson = new Gson();
-                        SocialReq social = gson.fromJson(response.body().toString(), SocialReq.class);
-                        wtf("Hulk-40", social.getResult().get(0).getMessage());
-                        response_data.setValue(response.body());
+                        BannersReq banners = gson.fromJson(response.body().toString(), BannersReq.class);
+
+                        response_data.setValue(banners);
                     } catch (Exception e) {
-                        wtf("Hulk-51uuis", e.getMessage());
+                        wtf("Hulk-36", e.getMessage());
+                        message.setValue("Service error while loading data");
                     }
 
                 } else {
                     //not got response
+                    message.setValue("No data to show");
                 }
             }
 
             @Override
             public void onFailure(Call<String> call, Throwable t) {
-                wtf("Hulk-63ds-err", t.getMessage());
+                wtf("Hulk-46-err", t.getMessage());
+                message.setValue("Error loading data");
+
             }
         });
     }
