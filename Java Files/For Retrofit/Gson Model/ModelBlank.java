a72422000cc3a960f25cab748c9cc8e5258f970b
okcoding21 <81430894+AppSync-ai@users.noreply.github.com>
2022-01-02 01:04:01 +0530

    Update ModelBlank.java

--- Java Files/For Retrofit/Gson Model/ModelBlank.java
+++ Java Files/For Retrofit/Gson Model/ModelBlank.java
@@ -1,39 +1,36 @@
 package wrteam.multivendor.shop.helper;
-
 import static android.util.Log.*;
 import static android.util.Log.wtf;
-
 import android.util.Log;
-
 import androidx.lifecycle.MutableLiveData;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 
 public class ModelBlank {
-    public static MutableLiveData<String> message = new MutableLiveData<>();
-    public static MutableLiveData<String> response_data = new MutableLiveData<>();
+
+    public static MutableLiveData<ArrayList<String>> response_data = new MutableLiveData<>();
 
     public static void load() {
+		response_data.setValue(null);
         JsonPlaceHolderAPI serviceGson = RetrofitClientInstanceGson.getRetrofitInstance().create(JsonPlaceHolderAPI.class);
-        Call<String> call = serviceGson.getUsers();
-        call.enqueue(new Callback<String>() {
+        Call<ArrayList<String>> call = serviceGson.getUsers();
+        call.enqueue(new Callback<ArrayList<String>>() {
             @Override
-            public void onResponse(Call<String> call, Response<String> response) {
+            public void onResponse(Call<ArrayList<String>> call, Response<ArrayList<String>> response) {
                 try {
 				response_data.setValue(response.body());
                 } catch (Exception e) {
                    wtf("Hulk-" + getClass().getSimpleName() + "-" + Admin.getLineNumber(), ""+t);
-                    message.setValue("No data to show");
+                    response_data.setValue(null);
                 }
             }
 
             @Override
-            public void onFailure(Call<String> call, Throwable t) {
+            public void onFailure(Call<ArrayList<String>> call, Throwable t) {
                 wtf("Hulk-" + getClass().getSimpleName() + "-" + Admin.getLineNumber(), ""+t);
-                message.setValue("Failed to load data");
+                response_data.setValue(null);
             }
         });
 
